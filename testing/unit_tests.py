d8cef7966ab08ad1cc3796128a048a6a1839c173
deralexxx <mail@alexanderjaeger.de>
2017-06-11 19:50:51 +0200

    respect the latest changes

--- testing/unit_tests.py
+++ testing/unit_tests.py
@@ -1,10 +1,18 @@
 import unittest
 
 from pyFireEyeAlert import *
+#from firemisp_settings import *
+
+import fmtest
 
 class PrimesTestCase(unittest.TestCase):
     """Tests for `primes.py`."""
 
+    def test_is_isight_working(self):
+        #serverurl = 'http://' + url + ':' + port
+
+        """Is test api there aka connection test"""
+        self.assertTrue(fmtest.processFile(inputfile='alert_details_fireeye_reducted.json',serverurl='http://127.0.0.1:8081'))
 
 
 if __name__ == '__main__':
